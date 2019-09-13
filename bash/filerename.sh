b46ec2e244346d4b33188bc5520f5917bf1cd449
David Chaid <dchaid@gmail.com>
2019-09-13 14:10:52 -0700

    Added filerename.sh

---
+++ bash/filerename.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+#removes text from filenames and removes trailing whitespace from end of title
+
+find . -name '*(USA)*' | while read f; do mv "$f" "${f//\(USA)/}"; done
+
+find . -name '*(USA, Europe)*' | while read f; do mv "$f" "${f//\(USA, Europe)/}"; done
+
+find . -name '*(World)*' | while read f; do mv "$f" "${f//\(World)/}"; done
+
+find . -name '*(Japan, USA)*' | while read f; do mv "$f" "${f//\(Japan, USA)/}"; done
+
+find . -name '*(Rev A)*' | while read f; do mv "$f" "${f//\(Rev A)/}"; done
+
+find . -name '*(En,Fr,De,It)*' | while read f; do mv "$f" "${f//\(En,Fr,De,It)/}"; done
+
+find . -name '*(Europe)*' | while read f; do mv "$f" "${f//\(Europe)/}"; done
+
+find . -name '*(Japan)*' | while read f; do mv "$f" "${f//\(Japan)/}"; done
+
+find . -name '*(J)*' | while read f; do mv "$f" "${f//\(J)/}"; done
+
+find . -name '*(EU)*' | while read f; do mv "$f" "${f//\(EU)/}"; done
+
+find . -name '*(\ .gb)*' | while read f; do mv "$f" "${f//\(.gb)/}"; done
+
+find . -type f | while read i; do mv "$i" "$(sed 's/ \.\([a-z0-9]*\)$/\.\1/' <<< "$i")"; done;
+
+exit 0
