4647922cc190dd5a9f8df2c940e006048c8938f3
David Chaid <dchaid@gmail.com>
2019-09-13 16:18:13 -0700

    Update test.sh

--- bash/test.sh
+++ bash/test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-b='/usr/local/bin/brew'
-#install brew dependencies
-echo "STARTING HOMEBREW INSTALLATIONS...";
-$b install ack;
-exit 0 ;
+printf "\033[48;5;5m"  # or "\033[46m" background
+printf "\033[H\033[2J" # your system's clear command does something similar
+printf "\u001b[43m" # or "\033[96m" text
+echo Hello World;
+echo;
+
+echo;
+eval ping www.google.com
+
+exit 0
