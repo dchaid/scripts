376c1fc42abd9e13b3945bb0a52b6f0eccfa39d3
David Chaid <dchaid@gmail.com>
2019-09-12 17:39:10 -0700

    removed extra ps1

--- getADUserPath.ps1
+++
@@ -1,4 +0,0 @@
-Get-ADUser -Filter 'HomeDrive -ne "$Local"' `
--Property Name,CanonicalName,CN,DisplayName,DistinguishedName,HomeDirectory,`
-HomeDrive,SamAccountName,UserPrincipalName | `
-export-csv -path (Join-Path $pwd HomeDrive.csv) -encoding ascii -NoTypeInformation
