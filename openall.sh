0ce3108c9fbf27fa10114c090d6637e962965fe0
David Chaid <dchaid@gmail.com>
2019-09-12 17:02:50 -0700

    Revert "Revert "powers on vm with cron job""
    This reverts commit 71b1de9bf6f2d8396377d53a8a99b29ac2b8e9da.

---
+++ openall.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+###################################################################################################
+#---Created by: David Chaid - KDInfotech
+#---Creation date: September 4, 2019
+#---Last Modified: September 4, 2019
+#---Modified by: David Chaid
+#---Modified for: Personal
+#---Description: Opens all apps
+###################################################################################################
+
+
+#---script
+cd /Applications/
+open 8x8\ -\ Virtual\ Office.app/ ;
+open Mail\ for\ Gmail.app ;
+open Stickies.app ;
+open Slack.app ;
+open Spotify.app ;
+open Firefox\ Developer\ Edition.app/ ;
+open Tweetbot.app ;
+open Airmail\ 3.app/ ;
+open 1Password\ 7.app &&
+open Messages.app ;
+open Visual\ Studio\ Code.app/ ;
+open iTerm.app ;
+open Twitch.app ;
+open Steam.app ;
+
+exit 0
---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+b='/usr/local/bin/brew'
+#install brew dependencies
+echo "STARTING HOMEBREW INSTALLATIONS...";
+$b install ack;
+exit 0 ;
