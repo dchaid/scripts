b73ad275a9c0e55c60f75eaae3d0afe722d41f78
David Chaid <dchaid@gmail.com>
2019-09-20 08:59:04 -0700

    added spinner

--- bash/ntfs.sh
+++ bash/ntfs.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
+#*****************************************************************************************************************************
+#functions
+#*****************************************************************************************************************************
+#cursor spin
+spin()
+{
+  spinner="/|\\—/|\\—"
+  while :
+  do
+    for i in `seq 0 7`
+    do
+      echo -n "${spinner:$i:1}"
+      echo -en "\010"
+      sleep .06
+    done
+  done
+}
+
 sudo -v
+
+#*****************************************************************************************************************************
+#script
+#*****************************************************************************************************************************
+
+# Start the Spinner + Make a note of its Process ID (PID)
+spin &
+SPIN_PID=$!
+trap "kill -9 $SPIN_PID" `seq 0 15`
 
 echo "STARTING COPY - PRESS CTRL + C TO QUIT"; sleep 5;
 rsync --update -rtzv --progress /Volumes/C /Volumes/tpupach/cdrive/ ;
@@ -8,6 +35,12 @@ date -u;
 echo "C-DRIVE SEGMENT COMPLETE";
 
 rsync --update -rtzv --progress /Volumes/D /Volumes/tpupach/ddrive/ ;
+
+eval clear;
+
+#kill spinner
+kill -9 $SPIN_PID;
+
 date -u;
 echo "D-DRIVE SEGMENT COMPLETE";
 
