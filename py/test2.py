e240414d10e7c315944de9fc9c8c03e4c9de8d69
David Chaid <dchaid@gmail.com>
2019-10-25 17:00:49 -0700

    .tsv updates

--- py/test2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-import csv
-
-with open('logs.tsv') as input, open('logs.tsv', 'w+') as output:
-    non_blank = (line for line in input if line.strip())
-    output.writelines(non_blank)
-
